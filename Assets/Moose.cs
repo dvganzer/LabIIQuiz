c7a2fa6d3ab89f52cd3f53b793d6db9f402895f7
Derek V Ganzer <63074878+dvganzer@users.noreply.github.com>
2021-02-23 15:58:09 -0500

    Initial commit

---
+++ Assets/Moose.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moose : MonoBehaviour
+{
+    public string playerName2;
+    public int playerLegal;
+
+    public Text tellPlayer;
+    public Slider mooseColor;
+    public Slider mooseSize;
+    public Slider mooseRotate;
+    public GameObject Mose;
+   
+
+    Color32[] Colors;
+    private void Awake()
+    {
+        Colors = new Color32[4];
+        Colors[0] = new Color32(185, 0, 185, 255);
+        Colors[1] = new Color32(0, 127, 255, 255);
+        Colors[2] = new Color32(0, 0, 255, 255);
+        Colors[3] = new Color32(0, 255, 255, 255);
+
+        playerName2 = PlayerPrefs.GetString("ThisPlayer", "playerName");
+        playerLegal = PlayerPrefs.GetInt("ThisAge");
+        tellPlayer.text = "Hi " + playerName2 + " your age is " + playerLegal;
+    }
+
+    public void changeColor()
+    {
+        float newMooseColor = mooseColor.value;
+        Mose.GetComponent<SpriteRenderer>().color = Colors[(int)newMooseColor];
+    }
+    public void changeSize()
+    {
+        float newMooseSize = mooseSize.value;
+        Mose.transform.localScale = new Vector2(newMooseSize, newMooseSize);
+    }
+    public void changeRotation()
+    {
+        float mooseRotValue = (mooseRotate.value * 90f) - 45;
+
+        Mose.transform.eulerAngles = new Vector3(0, 0, mooseRotValue);
+    }
+}
---
+++ Assets/SceneChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public InputField playerHandle;
+    public InputField playerAge;
+    public string playerName;
+    public int myAge; 
+    public void setName()
+    {
+        playerName = playerHandle.text;
+        
+
+        Debug.Log("name and age of player is " + playerName + myAge);
+
+        PlayerPrefs.SetString("ThisPlayer", playerName);
+       
+    } 
+    public void setAge()
+    {
+        myAge = int.Parse(playerAge.text);
+        PlayerPrefs.SetInt("ThisAge", myAge);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
